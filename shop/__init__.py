1de6ae3ab1a0c673e380e06a6395476ab9a5e51f
sam <samjordan427@gmail.com>
2019-03-10 11:36:13 +0000

    initial

---
+++ shop/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ec4e4c914bf29e11cc501a402d3ba0781031295f209414db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1834256:Chocolate64@csmysql.cs.cf.ac.uk:3306/c1834256'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from shop import routes
--- shop/routes.py
+++ shop/routes.py
@@ -1,8 +1,21 @@
 import os
-from flask import rener_template, url_for, request, redirect, flash
-from shop import app, debug
-#from shop.models import add in the database tables
-from shop.forms import RegistrationForm, LoginForm
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, url_for, request, redirect, flash
+from shop import app, db
+from shop.models import Manufacturer, Phone, User
+#from shop.forms import RegistrationForm, LoginForm
 from flask_login import login_user, current_user, logout_user, login_required
+
+@app.route("/")
+@app.route("/home")
+def home():
+    phones = Phone.query.all()
+    return render_template('home.html', phones=phones,)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About us')
+
+@app.route("/phone/<int:phone_id>")
+def book(book_id):
+    phone = phone.query.get_or_404(phone_id)
+    return render_template('book.html', name=phone.name, phone=phone)
